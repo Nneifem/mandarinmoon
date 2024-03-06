4aae78c89cfe80b5935bf5e97663a1cb41f76ee7
Nneif <nneifemakpunonu@gmail.com>
2024-03-05 22:42:51 -0500

    changed the background and added icons to each link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import mangaBkg from '../src/assets/styles/_.jpeg'
+import strawberryBkg from './assets/images/strawberry.jpg'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <div style={{ backgroundImage: `url(${strawberryBkg})`, backgroundSize: 'cover', width: '100vw', height: '100vh'}}>
     <Outlet />
+    </div>
     </>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,24 @@
 import '../assets/styles/home.css'
+import Instagram from '../assets/images/icons8-instagram-50.png'
+import Tiktok from '../assets/images/tiktok.png'
+import Youtube from '../assets/images/youtube.png'
 
 export default function Home(){
     return(
         <>
         <h1 className="name">Grey</h1>
+        <p className="bio">hello fellas</p>
             {/* <div className="text-center">
             <img src= className='img-fluid'></img>
                 </div>  */}
                 <section className="main-page">
-                <a href="https://www.tiktok.com/@mandarinmooon" className="tt">TikTok</a>
-                <a href="https://www.instagram.com/mandarinmoonn/" className="insta">Instagram</a>
-                    <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g" className="youtube">Youtube</a>
-                </section>   
+                <a href="https://www.tiktok.com/@mandarinmooon" className="tt">TikTok <img src={Tiktok} className='tiktok-pic' alt='tiktok logo'></img></a>
+                <a href="https://www.instagram.com/mandarinmoonn/" className="insta">Instagram <img src={Instagram} className='insta-pic' alt='instagram logo'></img></a>
+                <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g" className="youtube">Youtube <img src={Youtube} className='youtube-pic' alt='youtube logo'></img></a>
+                </section>
+                <footer className="footer">
+                    <p className="credits">made by me ᵔᴥᵔ</p>
+                </footer>   
         </>
     )
 }
