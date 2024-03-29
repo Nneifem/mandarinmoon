10bf281f9742966e7dd5b2d4aa7bf57ed6676363
Nneif <nneifemakpunonu@gmail.com>
2024-03-28 21:04:04 -0400

    fixed up some of the style

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,19 +3,14 @@ import Instagram from '../assets/images/icons8-instagram-50.png'
 import Tiktok from '../assets/images/tiktok.png'
 import Youtube from '../assets/images/youtube.png'
 import selfie from '../assets/images/selfie.png'
-import selfieTwo from '../assets/images/selfie2.png'
 
 export default function Home(){
     return(
         <>
         <section className='selfie-images'>
         <img src={selfie} className='img-fluid'></img>
-        <img src={selfieTwo} className='img-fluid'></img>
         </section>
         <h1 className="bio">grey</h1>
-            {/* <div className="text-center">
-            <img src= className='img-fluid'></img>
-                </div>  */}
                 <section className="main-page">
                 <a href="https://www.tiktok.com/@mandarinmooon" className="tt">TikTok <img src={Tiktok} className='tiktok-pic' alt='tiktok logo'></img></a>
                 <a href="https://www.instagram.com/mandarinmoonn/" className="insta">Instagram <img src={Instagram} className='insta-pic' alt='instagram logo'></img></a>
