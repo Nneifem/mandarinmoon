26769ed61bebd3c630cf61f486c8a7a2ee7e35ae
Nneif <nneifemakpunonu@gmail.com>
2024-10-30 13:09:49 -0400

    redesigning the page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,22 +2,28 @@ import '../assets/styles/home.css'
 import Instagram from '../assets/images/insta.png'
 import Tiktok from '../assets/images/tiktok.png'
 import Youtube from '../assets/images/youtube.png'
-import selfie from '../assets/images/tcos.jpg'
+import selfie from '../assets/images/pfp.png'
 
 export default function Home(){
     return(
         <>
-        <section className='selfie-images'>
+        <header className='selfie-images'>
         <img src={selfie} className='img-fluid'></img>
-        </section>
+        </header>
         <h1 className="name">grey</h1>
-
-        <section className="socials">
-            <a href="https://www.tiktok.com/@mandarinmooon"><img src={Tiktok} className="tt-img"></img><p>tiktok</p></a>
-            <a href="https://www.instagram.com/mandarinmoonn/"><img src={Instagram} className="insta-img"></img><p>instagram</p></a>
-            <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g"><img src={Youtube} className="yt-img"></img><p>youtube</p></a>
+        <section className='info'>
+            <a href="https://www.tiktok.com/@mandarinmooon"><p>Tiktok</p></a>
+            <a href="https://www.instagram.com/mandarinmoonn/"><p>Instagram</p></a>
+            <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g"><p>Youtube</p></a>
         </section>
-                
+
+        <footer>by mandarinmoonn</footer>
         </>
     )
 }
+
+{/* <section className="socials">
+            <a href="https://www.tiktok.com/@mandarinmooon"><img src={Tiktok} className="tt-img"></img><p>tiktok</p></a>
+            <a href="https://www.instagram.com/mandarinmoonn/"><img src={Instagram} className="insta-img"></img><p>instagram</p></a>
+            <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g"><img src={Youtube} className="yt-img"></img><p>youtube</p></a>
+        </section> */}
