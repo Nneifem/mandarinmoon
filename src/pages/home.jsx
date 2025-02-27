c8825355b6f4ec18a9f5779ca88820e5d6ed616b
Nneif <nneifemakpunonu@gmail.com>
2025-02-27 00:50:07 -0500

    added fading loading animation

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,18 +8,20 @@ import selfie from '../assets/images/chohan.png'
 export default function Home(){
     return(
         <>
-        <header className='selfie-images'>
-        <img src={selfie} className='img-fluid'></img>
-        </header>
-        <h1 className="name">˚ʚ grey ɞ˚</h1>
-        <section className='socials'>
-            <a href="https://www.instagram.com/mandarinmoonn/"><img src={Instagram} className='media'></img></a>
-            <a href="https://www.tiktok.com/@mandarinmooon"><img src={Tiktok} className='media'></img></a>
-            <a href="https://bsky.app/profile/mandarinmoonn.bsky.social"><img src={Bluesky} className='media'></img></a>
-            <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g"><img src={Youtube} className='media'></img></a>
+        <section className="fadeInUp-animation">
+            <header className='selfie-images'>
+            <img src={selfie} className='img-fluid'></img>
+            </header>
+            <h1 className="name">˚ʚ grey ɞ˚</h1>
+            <section className='socials'>
+                <a href="https://www.instagram.com/mandarinmoonn/"><img src={Instagram} className='media'></img></a>
+                <a href="https://www.tiktok.com/@mandarinmooon"><img src={Tiktok} className='media'></img></a>
+                <a href="https://bsky.app/profile/mandarinmoonn.bsky.social"><img src={Bluesky} className='media'></img></a>
+                <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g"><img src={Youtube} className='media'></img></a>
+            </section>
+            
+            <footer>dm on insta for collab</footer>
         </section>
-
-        <footer>dm on insta for collab</footer>
         </>
     )
 }
