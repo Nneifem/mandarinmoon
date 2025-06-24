7a8c1434b44de1ea5515eb1b558546afa1c72930
Nneif <nneifemakpunonu@gmail.com>
2025-06-24 17:30:35 -0400

    added social media boxes and small description

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,6 @@
 import '../assets/styles/home.css'
 import Instagram from '../assets/images/insta.png'
 import Tiktok from '../assets/images/tiktok.png'
-import Bluesky from '../assets/images/bluesky.png'
 import Youtube from '../assets/images/youtube.png'
 import selfie from '../assets/images/chohan.png'
 
@@ -13,11 +12,28 @@ export default function Home(){
             <img src={selfie} className='img-fluid'></img>
             </header>
             <h1 className="name">˚ʚ grey ɞ˚</h1>
+            <section className="subtitle">
+                <p>northeast cosplayer <br></br> I dress up as my fav characters</p>
+            </section>
+
             <section className='socials'>
                 <a href="https://www.instagram.com/mandarinmoonn/"><img src={Instagram} className='media'></img></a>
                 <a href="https://www.tiktok.com/@mandarinmooon"><img src={Tiktok} className='media'></img></a>
-                <a href="https://bsky.app/profile/mandarinmoonn.bsky.social"><img src={Bluesky} className='media'></img></a>
                 <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g"><img src={Youtube} className='media'></img></a>
+            </section>
+
+            <section className='social-boxes'>
+                <section className='insta-box'>
+                    <a href="https://www.instagram.com/mandarinmoonn/"><img src={Instagram} className='media' ></img>Instagram</a>
+                </section>
+
+                <section className='tt-box'>
+                <a href="https://www.tiktok.com/@mandarinmooon"><img src={Tiktok} className='media'></img>TikTok</a>
+                </section>
+
+                <section className='yt-box'>
+                <a href="https://www.youtube.com/channel/UCPzmpuYVIXpHKUIPUEB8m0g"><img src={Youtube} className='media'></img>YouTube</a>
+                </section>
             </section>
             
             <footer>dm on insta for collab</footer>
